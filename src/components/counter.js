a54dc0e4843c98133a929b63778fcad141ae9197
Regina Battle <hello@reginabattle.com>
2019-03-18 19:15:59 -0400

    Create counter component

---
+++ src/components/counter.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+	render() {
+			<div>
+				<h1>Hello world</h1>
+				<button>Increment</button>
+			</div>
+		)
+	}
+}
+
+export default Counter;
