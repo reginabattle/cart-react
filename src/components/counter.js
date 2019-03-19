f3c0e3f5db33fe39ba61e3e4ea8657ebcbba175e
Regina Battle <hello@reginabattle.com>
2019-03-18 21:14:08 -0400

    Add boostrap

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+
+	// Set inital state - contains data the component needs
+	state = {
+		count: 0,
+	};
+
 	render() {
-			<div>
-				<h1>Hello world</h1>
-				<button>Increment</button>
+
+		let classes = this.getBadgeClasses();
+
+		return (
+			<div className="container pt-5">
+				<span className={ classes }>{ this.formatCount() }</span>
+				<button className="btn btn-secondary btn-sm">Increment</button>
 			</div>
-		)
+		);
 	}
+
+	// Check the value of the count property
+	formatCount() {
+		const { count } = this.state;
+		return count === 0 ? 'Zero' : count; 
+	}
+
+	// Get badge classes 
+	getBadgeClasses() {
+		let classes = "badge m-2 badge-";
+		classes += this.state.count === 0 ? 'warning' : 'info';
+		return classes;
+	}
+
 }
 
 export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import Counter from './components/counter';
 import * as serviceWorker from './serviceWorker';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(<Counter />, document.getElementById('root'));
 
