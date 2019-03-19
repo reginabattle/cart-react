af6a94b3c51de6db9897ad5c45a47d82211a2d0a
Regina Battle <hello@reginabattle.com>
2019-03-19 11:29:42 -0400

    Clean up and remove files

--- READEME.md
+++
@@ -1,8 +0,0 @@
-# Cart
-A shopping cart built with React.
-
-## Setup
-
-In the project directory, install npm and run:
-
-### `npm start`
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import Counter from './components/counter';
-
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css'
-
 
 ReactDOM.render(<Counter />, document.getElementById('root'));
