c18651d9bf7ebc9fc9cbf8d9f2c6740e20570cb0
Regina Battle <hello@reginabattle.com>
2019-03-18 22:05:59 -0400

    Add event handler to increment button

--- src/components/counter.js
+++ src/components/counter.js
@@ -7,22 +7,17 @@ class Counter extends Component {
 		count: 0,
 	};
 
-	render() {
-
-		let classes = this.getBadgeClasses();
-
-		return (
-			<div className="container pt-5">
-				<span className={ classes }>{ this.formatCount() }</span>
-				<button className="btn btn-secondary btn-sm">Increment</button>
-			</div>
-		);
+	handleIncrement() {
+		console.log('Increment clicked');
 	}
 
-	// Check the value of the count property
-	formatCount() {
-		const { count } = this.state;
-		return count === 0 ? 'Zero' : count; 
+	render() {
+		return (
+			<div className="container pt-5">
+				<span className={ this.getBadgeClasses() }>{ this.formatCount() }</span>
+				<button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+			</div>
+		);
 	}
 
 	// Get badge classes 
@@ -30,6 +25,12 @@ class Counter extends Component {
 		let classes = "badge m-2 badge-";
 		classes += this.state.count === 0 ? 'warning' : 'info';
 		return classes;
+	}
+
+	// Check the value of the count property
+	formatCount() {
+		const { count } = this.state;
+		return count === 0 ? 'Zero' : count; 
 	}
 
 }
