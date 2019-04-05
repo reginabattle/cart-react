763d70f1b87d2606a8cb557aa60b0c868a21c152
Regina Battle <hello@reginabattle.com>
2019-04-05 08:41:15 -0400

    Convert nav to stateless component

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 
-class Nav extends Component {
-	render() {
-		return (
-			<nav className="navbar navbar-light bg-light">
-			  <a className="navbar-brand" href="#">
-			  	Navbar <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
-			  </a>
-			</nav>
-		)
-	}
+const Nav = (props) => {
+	return (
+		<nav className="navbar navbar-light bg-light">
+		  <a className="navbar-brand" href="#">
+		  	Navbar 
+		  	<span className="badge badge-pill badge-secondary">{props.totalCounters}</span>
+		  </a>
+		</nav>
+	);
 }
 
 export default Nav;
